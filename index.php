de28784c9e8ee0ad8ec0524f4cf0c2ed6caff644
aidudu99 <zhangling_buaa@sina.com>
2017-09-03 21:38:57 +0800

    index file and css

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SVV Project Information</title>
+
+<link href="file:///lampp/htdocs/MyAPP/SVVSharing/CSS/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+	
+<div id="main" align="center">
+	<div id="header">SVV Project Information</div>	  
+	<div id="index">
+		<div id="index_top">
+			<div id="indexText" align="left">projects index</div>
+			<input id="addButton" type="button" align="right" value="Add">
+
+		
+		</div>
+	  <div id="index_bottom">project name</div>
+	</div>
+	<div id="contain">show project detail information here</div>	
+</div>
+
+</body>
+</html>
